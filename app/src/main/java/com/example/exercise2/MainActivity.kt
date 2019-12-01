2878eb2a50bbb2632f224c069e92de6a06cc1d5e
kianhee <n.kianhee99@gmail.com>
2019-12-01 15:58:37 +0800

    Exercise 2: BMI Calculator -Enhanced version

--- app/src/main/java/com/example/exercise2/MainActivity.kt
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.exercise2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,22 +15,27 @@ class MainActivity : AppCompatActivity() {
         calculateBMI.setOnClickListener{calculateBMI()}
         reset.setOnClickListener{reset()}
     }
-    fun calculateBMI(){
+    private fun calculateBMI(){
         val weightInKg : EditText = findViewById(R.id.editTextWeight)
         val heightInCm : EditText = findViewById(R.id.editTextHeight)
-        val heightInMeter : Double = heightInCm.text.toString().toDouble() / 100
-        val bmi : Double = weightInKg.text.toString().toDouble() / (heightInMeter * heightInMeter)
-        val bmiText : TextView = findViewById(R.id.textViewBMI)
-        bmiText.text = String.format("%s %.2f",getString(R.string.bmi),bmi)
-        val imageBMI : ImageView = findViewById(R.id.imageViewProfile)
-        if(bmi<18.5)
-        {
-            imageBMI.setBackgroundResource(R.drawable.under)
-        }else if(bmi in 18.5..24.9){
-            imageBMI.setBackgroundResource(R.drawable.normal)
-        }
-        else if(bmi>25){
-            imageBMI.setBackgroundResource(R.drawable.over)
+        if(weightInKg.text.isEmpty() || heightInCm.text.isEmpty()){
+            Toast.makeText(this, getString(R.string.input_error),
+                Toast.LENGTH_SHORT).show()
+        }else{
+            val heightInMeter : Double = heightInCm.text.toString().toDouble() / 100
+            val bmi : Double = weightInKg.text.toString().toDouble() / (heightInMeter * heightInMeter)
+            val bmiText : TextView = findViewById(R.id.textViewBMI)
+            bmiText.text = String.format("%s %.2f",getString(R.string.bmi),bmi)
+            val imageBMI : ImageView = findViewById(R.id.imageViewProfile)
+            if(bmi<18.5)
+            {
+                imageBMI.setBackgroundResource(R.drawable.under)
+            }else if(bmi in 18.5..24.9){
+                imageBMI.setBackgroundResource(R.drawable.normal)
+            }
+            else if(bmi>25){
+                imageBMI.setBackgroundResource(R.drawable.over)
+            }
         }
     }
 
